1bc048de5f2b4795e1c54393e4e8605797022d1d
Patrick <patrick.rdecarvalho@outlook.com>
2022-02-15 15:57:30 -0300

    feature: Primeira aula do NLW HEAT

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express from 'express';
+import { router } from "./routes";
+
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
+
+const app = express();
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+});
+
+app.use(express.json());
+app.use(router);
+
+export { serverHttp, io}
