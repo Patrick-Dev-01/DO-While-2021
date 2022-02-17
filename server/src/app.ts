7c3135ee67c55790e5a266108ecfc2130eade55b
Patrick <patrick.rdecarvalho@outlook.com>
2022-02-17 13:39:03 -0300

    feature: Aplicação web / frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -20,6 +20,7 @@ io.on("connection", (socket) => {
 });
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 export { serverHttp, io}
--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import prismaClient from '../prisma';
 import { sign } from 'jsonwebtoken';
 
-interface IAcessTokenResponse{
+interface IAccessTokenResponse{
     access_token: string;
 }
 
@@ -17,7 +17,7 @@ class AuthenticateUserService{
     async execute(code: string){
         const url = "http://github.com/login/oauth/access_token";
 
-        const { data: accessTokenResponse } = await axios.post<IAcessTokenResponse>(url, null, {
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
